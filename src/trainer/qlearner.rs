adacd6cf8968e70fca95bb8a373c5e97e2291cab
NivenT <nachenjang@gmail.com>
2016-11-17 01:02:30 -0800

    Added DynaQ learning algorithm

--- src/trainer/qlearner.rs
+++ src/trainer/qlearner.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+use std::hash::Hash;
 use std::f64;
 
 use rand::{thread_rng, Rng};
@@ -7,7 +9,8 @@ use environment::Transition;
 use environment::{Space, FiniteSpace};
 use trainer::{OnlineTrainer, Model};
 use agent::Agent;
-use util::{QFunction, TimePeriod};
+use util::{QFunction, TimePeriod, Chooser};
+use util::chooser::Weighted;
 
 /// Represents an OnlineTrainer for Q-functions
 /// Uses the [Q-learning algorithm](https://www.wikiwand.com/en/Q-learning)
@@ -159,7 +162,8 @@ impl SARSALearner {
 /// Represents an OnlineTrainer for Q-functions
 /// Uses the Dyna-Q
 #[derive(Debug)]
-pub struct DynaQ<S: Space, A: FiniteSpace, M: Model<S, A>> {
+pub struct DynaQ<S: Space, A: FiniteSpace, M: Model<S, A>> where
+	S::Element: Hash + Eq, A::Element: Hash + Eq {
 	/// External trainer for updating Q 
 	all_actions: Vec<A::Element>,
 	/// The discount factor
@@ -171,14 +175,18 @@ pub struct DynaQ<S: Space, A: FiniteSpace, M: Model<S, A>> {
 	/// The number of (state, action) pairs to sample each train step
 	num_samples: usize,
 	/// The states observed by the agent
-	states: Vec<S::Element>,
+	states: HashSet<S::Element>,
 	/// The actions performed by the agent
-	actions: Vec<A::Element>,
+	actions: HashSet<A::Element>,
 	model: M,
 }
 
 impl<T, S: Space, A: FiniteSpace, M: Model<S, A>> OnlineTrainer<S, A, T> for DynaQ<S, A, M>
-	where T: QFunction<S, A> + Agent<S, A> {
+	where T: QFunction<S, A> + Agent<S, A>,
+		  S::Element: Hash + Eq,
+		  A::Element: Hash + Eq {
+	// Possible improperly implemented
+	// Updating Q maybe should make heavier use of model
 	fn train_step(&mut self, agent: &mut T, transition: Transition<S, A>) {
 		let (state, action, reward, next) = transition;
 		
@@ -190,13 +198,26 @@ impl<T, S: Space, A: FiniteSpace, M: Model<S, A>> OnlineTrainer<S, A, T> for Dyn
 		agent.update(state, action, reward + self.gamma*max_next_val, self.alpha);
 		self.model.update(transition);
 
-		self.states.push(state.clone());
-		self.actions.push(action.clone());
+		self.states.insert(state.clone());
+		self.states.insert(next.clone());
+		self.actions.insert(action.clone());
 
 		let mut rng = thread_rng();
 		for _ in 0..self.num_samples {
-			let s = &self.states[rng.gen_range(0, self.states.len())];
-			let a = &self.actions[rng.gen_range(0, self.actions.len())];
+			let state_index = rng.gen_range(0, self.states.len());
+			let s0 = &self.states.iter().skip(state_index).next().unwrap();
+
+			let action_index = rng.gen_range(0, self.actions.len());
+			let a = &self.actions.iter().skip(action_index).next().unwrap();
+
+			let (r, s1) = self.sample_model(s0, a);
+
+			let mut max_next_val = f64::MIN;
+			for a in &self.all_actions {
+				max_next_val = max_next_val.max(agent.eval(s1, a));
+			}
+
+			agent.update(s0, a, r + self.gamma*max_next_val, self.alpha);			
 		}
 	}
 	fn train(&mut self, agent: &mut T, env: &mut Environment<State=S, Action=A>) {
@@ -213,7 +234,8 @@ impl<T, S: Space, A: FiniteSpace, M: Model<S, A>> OnlineTrainer<S, A, T> for Dyn
 	}
 }
 
-impl<S: Space, A: FiniteSpace, M: Model<S, A>> DynaQ<S, A, M> {
+impl<S: Space, A: FiniteSpace, M: Model<S, A>> DynaQ<S, A, M> 
+	where S::Element: Hash + Eq, A::Element: Hash + Eq {
 	/// Returns a new DynaQ with the given parameters
 	pub fn new(action_space: A, gamma: f64, alpha: f64, train_period: TimePeriod, 
 				num_samples: usize, model: M) -> DynaQ<S, A, M> {
@@ -223,8 +245,21 @@ impl<S: Space, A: FiniteSpace, M: Model<S, A>> DynaQ<S, A, M> {
 			alpha: alpha,
 			train_period: train_period,
 			num_samples: num_samples,
-			states: vec![],
-			actions: vec![],
+			states: HashSet::new(),
+			actions: HashSet::new(),
+			model: model
+		}
+	}
+	/// Creates a new DynaQ with default parameters
+	pub fn default(action_space: A, model: M) -> DynaQ<S, A, M> {
+		DynaQ {
+			all_actions: action_space.enumerate(),
+			gamma: 0.95,
+			alpha: 0.1,
+			train_period: TimePeriod::EPISODES(30),
+			num_samples: 10,
+			states: HashSet::new(),
+			actions: HashSet::new(),
 			model: model
 		}
 	}
@@ -249,7 +284,15 @@ impl<S: Space, A: FiniteSpace, M: Model<S, A>> DynaQ<S, A, M> {
 		self
 	}
 
-	fn sample_model(&self) -> (f64, S::Element) {
-		(0.0, self.states[0].clone())
+	fn sample_model(&self, s: &S::Element, a: &A::Element) -> (f64, &S::Element) {
+		let weights = self.states.iter().map(|s1| {
+			self.model.transition(s, a, s1)
+		}).collect::<Vec<_>>();
+		let states: Vec<_> = self.states.iter().collect();
+
+		let chooser = Weighted;
+		let s1 = chooser.choose(states, weights);
+		let r = self.model.reward(s, a, s1);
+		(r, s1)
 	}
 }
